7696fe0c642a86af164d2803419cf8e655a07d06
enzozal23 <enzo.z.1.ez@gmail.com>
2024-10-09 00:19:28 -0300

    add api

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,5 @@
 import axios from './axios';
-const API = 'https://zachpage.onrender.com'
+const API = 'https://zachpage.onrender.com/api'
 
 export const registerRequest = user => axios.post(`/register`, user)
 export const loginRequest = user => axios.post(`/login`, user)
