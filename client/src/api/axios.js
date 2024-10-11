e8abd966c42a5e94392030a9cd737c43125653bb
enzozal23 <enzo.z.1.ez@gmail.com>
2024-10-10 23:33:28 -0300

    correccion ruta

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'https://zachpage.onrender.com',
+    baseURL: 'https://zachpage.onrender.com/',
     withCredentials: true
 })
 export default instance
